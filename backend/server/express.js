7694001e346f4ec2ccfd6f97c2f61022869add6f
Alumne DAM <guillemruano06@gmail.com>
2025-05-26 09:07:16 +0200

    endpoint usuarios

--- backend/server/express.js
+++ backend/server/express.js
@@ -25,6 +25,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'..' ,'..', 'frontend', 'html', 'check.html'));
 });
 
+//proyectos
 app.get('/api/projects', async (req, res) => {
     const apiKey = req.headers['x-api-key'];
 
@@ -50,6 +51,35 @@ app.get('/api/projects', async (req, res) => {
         res.status(500).json({ error: 'Server error' });
     }
 });
+
+
+//usuarios
+app.get('/api/users', async (req, res) => {
+    const apiKey = req.headers['x-api-key'];
+
+    if (!apiKey) {
+        return res.status(400).json({ error: 'API key is missing in headers' });
+    }
+
+    try {
+        const response = await fetch('http://localhost:8080/api/v3/users', {
+            headers: {
+                'Authorization': 'Basic ' + Buffer.from(`apikey:${apiKey}`).toString('base64')
+            }
+        });
+
+        if (!response.ok) {
+            return res.status(response.status).json({ error: 'Failed to fetch from OpenProject API' });
+        }
+
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching from OpenProject:', error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor escoltant a http://localhost:${PORT}`);
 });
